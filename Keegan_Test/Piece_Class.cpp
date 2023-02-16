4fc6eb0a627604bf4cd098ea77515e7294c7996d
sandke <sandke@cs.wallawalla.edu>
2023-02-16 20:02:59 +0000

    Adding new file

---
+++ Keegan_Test/Piece_Class.cpp
@@ -0,0 +1,89 @@
+
+#include <iostream>
+#include <tuple>
+#include <vector>
+using std::string;
+
+class ChessPieces 
+{
+private:
+bool team;
+
+public:
+    ChessPieces(bool color) {team = color;};
+
+    std::vector<std::tuple<int,int>> move(std::tuple<int> current_cell, std::vector<string> current_board) 
+    {
+        std::vector<std::tuple<int,int>> moves;
+        moves.push_back(std::make_tuple(0,0));
+        moves.push_back(std::make_tuple(0,7));
+        moves.push_back(std::make_tuple(7,0));
+        moves.push_back(std::make_tuple(7,7));
+        moves.push_back(std::make_tuple(4,0));
+        moves.push_back(std::make_tuple(4,7));
+        moves.push_back(std::make_tuple(3,0));
+        moves.push_back(std::make_tuple(3,7));
+        return moves;
+    }
+};
+
+
+
+class Pawn : public ChessPieces 
+{
+private:
+string name = "pawn";
+
+public:
+    Pawn(bool team);
+    
+};
+    // Pawn::Pawn(bool team) {
+    //     super(team);
+    // }
+
+
+class Knight : public ChessPieces 
+{
+private:
+string name = "knight";
+
+public:
+    
+};
+
+class Bishop : public ChessPieces 
+{
+private:
+string name = "bishop";
+
+public:
+    
+};
+
+class Queen : public ChessPieces 
+{
+private:
+string name = "queen";
+
+public:
+    
+};
+
+class King : public ChessPieces 
+{
+private:
+string name = "king";
+
+public:
+    
+};
+
+class Rook : public ChessPieces 
+{
+private:
+string name = "rook";
+
+public:
+    
+};
