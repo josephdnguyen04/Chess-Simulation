01ca70cc79eb115785c64f9ae4606b1918ebdf24
peteco <peteco@cs.wallawalla.edu>
2023-02-22 15:26:15 +0000

    autosave

--- project/chess_bot_header.h
+++ project/chess_bot_header.h
@@ -5,7 +5,17 @@
 #pragma once
 #include <string>
 #include <vector>
+using namespace std;
 
 // makeBoard() returns the initial 2-d vector of chess with pieces (as strings)
 // in their starting positions
-std::vector<std::vector<std::string>> makeBoard();
+vector<vector<string>> makeBoard();
+
+// takeTurn() should be a void function that passes the current chess board vector by reference to edit that board. The second parameter is for the color of piece to move. It should prompt the player for a move and then move accordingly.
+void takTurn(vector<vector<string>>&, string color);
+
+// comTurn() should be similar to takeTurn(), but the turn will be completed by the computer.
+void comTurn(vector<vector<string>>&, string color);
+
+// printBoard() is a void function with the current chess board as a const reference. It should cout ascii art to display the board.
+void printBoard(const vector<vector<string>>&);
