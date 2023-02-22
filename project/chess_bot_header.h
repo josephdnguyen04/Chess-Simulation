20d29b797d4a11eab59d2370fd0c5719c79ff495
peteco <peteco@cs.wallawalla.edu>
2023-02-22 15:05:24 +0000

    autosave

--- project/chess_bot_header.h
+++ project/chess_bot_header.h
@@ -1,9 +1,11 @@
-//This file is the header for the chess bot project
-//Please place all public function headers in here
-//With a comment description of what the function does
+// This file is the header for the chess bot project
+// Please place all public function headers in here
+// With a comment description of what the function does
 
 #pragma once
-#include <vector>
 #include <string>
+#include <vector>
 
-std::vector<std::vector<std::string>> makeBoard(); //makeBoard() returns the initial 2-d vector of chess with pieces (as strings) in their starting positions
+// makeBoard() returns the initial 2-d vector of chess with pieces (as strings)
+// in their starting positions
+std::vector<std::vector<std::string>> makeBoard();
