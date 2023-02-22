d61e7b3754b25bad9ee5df36448bdb2e839ed3d5
peteco <peteco@cs.wallawalla.edu>
2023-02-22 09:10:20 +0000

    autosave

--- project/chess_bot_board.cpp
+++ project/chess_bot_board.cpp
@@ -2,43 +2,42 @@
 using namespace std;
 
 vector<vector<string>> makeBoard() {
-    vector<vector<string>> board;
-    //initialize with blank spaces
-    for (int i = 0; i < 8; i++) {
-        vector<string> row;
-        for (int j = 0; j < 8; j++) {
-            row.push_back("_");
-        }
-        board.push_back(row);
+  vector<vector<string>> board;
+  // initialize with blank spaces
+  for (int i = 0; i < 8; i++) {
+    vector<string> row;
+    for (int j = 0; j < 8; j++) {
+      row.push_back("_");
     }
+    board.push_back(row);
+  }
 
-//replace some blank spaces with piece descriptions for the initial setup
-board.at(0).at(0) = "wRook";
-board.at(0).at(1) = "wKnight";
-board.at(0).at(2) = "wBishop";
-board.at(0).at(3) = "wQueen";
-board.at(0).at(4) = "wKing";
-board.at(0).at(5) = "wBishop";
-board.at(0).at(6) = "wKnight";
-board.at(0).at(7) = "wRook";
+  // replace some blank spaces with piece descriptions for the initial setup
+  board.at(0).at(0) = "wRook";
+  board.at(0).at(1) = "wKnight";
+  board.at(0).at(2) = "wBishop";
+  board.at(0).at(3) = "wQueen";
+  board.at(0).at(4) = "wKing";
+  board.at(0).at(5) = "wBishop";
+  board.at(0).at(6) = "wKnight";
+  board.at(0).at(7) = "wRook";
 
-for (unsigned int i = 0; i < board.at(0).size(); i++) {
+  for (unsigned int i = 0; i < board.at(0).size(); i++) {
     board.at(1).at(i) = "wPawn";
-}
+  }
 
-for (unsigned int i = 0; i < board.at(6).size(); i++) {
+  for (unsigned int i = 0; i < board.at(6).size(); i++) {
     board.at(6).at(i) = "bPawn";
-}
+  }
 
-board.at(7).at(0) = "bRook";
-board.at(7).at(1) = "bKnight";
-board.at(7).at(2) = "bBishop";
-board.at(7).at(3) = "bQueen";
-board.at(7).at(4) = "bKing";
-board.at(7).at(5) = "bBishop";
-board.at(7).at(6) = "bKnight";
-board.at(7).at(7) = "bRook";
+  board.at(7).at(0) = "bRook";
+  board.at(7).at(1) = "bKnight";
+  board.at(7).at(2) = "bBishop";
+  board.at(7).at(3) = "bQueen";
+  board.at(7).at(4) = "bKing";
+  board.at(7).at(5) = "bBishop";
+  board.at(7).at(6) = "bKnight";
+  board.at(7).at(7) = "bRook";
 
-
-    return board;
+  return board;
 }
--- project/chess_bot_main.cpp
+++ project/chess_bot_main.cpp
@@ -1,16 +1,22 @@
-#include <iostream>
-#include <vector>
-#include <string>
 #include "chess_bot_header.h"
+#include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
 int main() {
-    vector<vector<string>> board = makeBoard();
-for (int i = 0; i < board.size(); i++) {
-    for (int j = 0; j < board.at(i).size(); j++) {
-        cout << board.at(i).at(j) << ", ";
-    }
-    cout << endl;
-}
-    return 0;
+  vector<vector<string>> board = makeBoard();
+  int numPlayers = 0;
+  cout << "Welcome to Chess." << endl;
+  cout << "How many players are there? ";
+  cin >> numPlayers;
+  while (!(numPlayers == 0 || numPlayers == 1 || numPlayers == 2)) {
+    cout << "Sorry, " << numPlayers << " is an invalid number of players."
+         << endl;
+    cout << "Please enter an integer from 0 to 2." << endl;
+    cout << "How many players are there? ";
+    cin >> numPlayers;
+  }
+
+  return 0;
 }
