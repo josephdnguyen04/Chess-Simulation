791b7c6ff9b0c1bb0ee78ffd100014085f9e3742
sandke <sandke@cs.wallawalla.edu>
2023-02-23 19:07:46 +0000

    autosave

--- project/Piece_Class.cpp
+++ project/Piece_Class.cpp
@@ -4,86 +4,71 @@
 #include <vector>
 using std::string;
 
-class ChessPieces 
-{
+class ChessPieces {
 private:
-bool team;
+  bool team;
 
 public:
-    ChessPieces(bool color) {team = color;};
+  ChessPieces(bool color) { team = color; };
 
-    std::vector<std::tuple<int,int>> move(std::tuple<int> current_cell, std::vector<std::vector<string>> current_board) 
-    {
-        std::vector<std::tuple<int,int>> moves;
-        moves.push_back(std::make_tuple(0,0));
-        moves.push_back(std::make_tuple(0,7));
-        moves.push_back(std::make_tuple(7,0));
-        moves.push_back(std::make_tuple(7,7));
-        moves.push_back(std::make_tuple(4,0));
-        moves.push_back(std::make_tuple(4,7));
-        moves.push_back(std::make_tuple(3,0));
-        moves.push_back(std::make_tuple(3,7));
-        return moves;
-    }
+  std::vector<std::tuple<int, int>>
+  move(std::tuple<int> current_cell,
+       std::vector<std::vector<string>> current_board) {
+    std::vector<std::tuple<int, int>> moves;
+    moves.push_back(std::make_tuple(0, 0));
+    moves.push_back(std::make_tuple(0, 7));
+    moves.push_back(std::make_tuple(7, 0));
+    moves.push_back(std::make_tuple(7, 7));
+    moves.push_back(std::make_tuple(4, 0));
+    moves.push_back(std::make_tuple(4, 7));
+    moves.push_back(std::make_tuple(3, 0));
+    moves.push_back(std::make_tuple(3, 7));
+    return moves;
+  }
 };
 
-
-
-class Pawn : public ChessPieces 
-{
+class Pawn : public ChessPieces {
 private:
-string name = "pawn";
+  string name = "pawn";
 
 public:
-    Pawn(bool team);
-    
+  Pawn(bool team);
 };
-    // Pawn::Pawn(bool team) {
-    //     super(team);
-    // }
+// Pawn::Pawn(bool team) {
+//     super(team);
+// }
 
-
-class Knight : public ChessPieces 
-{
+class Knight : public ChessPieces {
 private:
-string name = "knight";
+  string name = "knight";
 
 public:
-    
 };
 
-class Bishop : public ChessPieces 
-{
+class Bishop : public ChessPieces {
 private:
-string name = "bishop";
+  string name = "bishop";
 
 public:
-    
 };
 
-class Queen : public ChessPieces 
-{
+class Queen : public ChessPieces {
 private:
-string name = "queen";
+  string name = "queen";
 
 public:
-    
 };
 
-class King : public ChessPieces 
-{
+class King : public ChessPieces {
 private:
-string name = "king";
+  string name = "king";
 
 public:
-    
 };
 
-class Rook : public ChessPieces 
-{
+class Rook : public ChessPieces {
 private:
-string name = "rook";
+  string name = "rook";
 
 public:
-    
 };
