ffc6d13448a3ec41d0f4eae8031ea6214883cf1e
nguyjo <nguyjo@cs.wallawalla.edu>
2023-02-27 05:39:26 +0000

    autosave

--- project/chess_bot_print_board.cpp
+++ project/chess_bot_print_board.cpp
@@ -1,10 +1,17 @@
 #include "chess_bot_header.h"
 
 void printBoard(const vector<vector<string>> &board) {
-    for (int i  = 0; i < board.size(); i++) {
+    int num = 8;
+    for (int i  = 0; i < board.size(); i++, num--) {
+        cout << num << " |";
         for (int j = 0; j < board.at(i).size(); j++) {
-            cout << board.at(i).at(j) << " ";
+            cout << board.at(i).at(j) << "|";
         }
         cout << endl;
+        if (i != 7) {
+            cout << "   -- -- -- -- -- -- -- --" << endl;
+        }
     }
+    cout << "   a  b  c  d  e  f  g  h" << endl;
+
 }
