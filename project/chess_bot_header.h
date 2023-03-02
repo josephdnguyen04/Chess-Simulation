41166e4812e016642f206b663323b9d334abba63
peteco <peteco@cs.wallawalla.edu>
2023-03-02 18:13:42 +0000

    autosave

--- project/chess_bot_header.h
+++ project/chess_bot_header.h
@@ -3,9 +3,9 @@
 // With a comment description of what the function does
 
 #pragma once
+#include <iostream>
 #include <string>
 #include <vector>
-#include <iostream>
 using namespace std;
 
 // makeBoard() returns the initial 2-d vector of chess with pieces (as strings)
@@ -16,23 +16,25 @@ vector<vector<string>> makeBoard();
 // vector by reference to edit that board. The second parameter is for the color
 // of piece to move, either "white" or "black". It should prompt the player for
 // a move and then move accordingly.
-// the definition is in chess_bot_turn.cpp 
+// the definition is in chess_bot_turn.cpp
 // THERE IS CURRENTLY AN ERROR WHERE MAIN DOES NOT LIKE THE FUNCTION.
-void takeTurn(vector<vector<string>> &board, string color); //Keegan
+void takeTurn(vector<vector<string>> &board, string color); // Keegan
 
-// converts a string of 2 character, where the first is a MAYUS letter and the second character must be a number
-// it returns a tuple with the letter converted into a number where A = 0, B = 1 and so on.
-// it should be used to convert user input into rows and columns
-// the definition is in chess_bot_turn.cpp 
-tuple<int,int> convertInput(string input); 
+// converts a string of 2 character, where the first is a MAYUS letter and the
+// second character must be a number it returns a tuple with the letter
+// converted into a number where A = 0, B = 1 and so on. it should be used to
+// convert user input into rows and columns the definition is in
+// chess_bot_turn.cpp
+tuple<int, int> convertInput(string input);
 
 // comTurn() should be similar to takeTurn(), but the turn will be completed by
 // the computer.
-void comTurn(vector<vector<string>> &board, string color); //no one should be working on this yet
+void comTurn(vector<vector<string>> &board,
+             string color); // no one should be working on this yet
 
 // printBoard() is a void function with the current chess board as a const
 // reference. It should cout ascii art to display the board.
-void printBoard(const vector<vector<string>> &board); //Joseph
+void printBoard(const vector<vector<string>> &board); // Joseph
 
 // checkEndGame() is an integer function that returns integers based on the game
 // state. It takes the chess board vector as a const reference.
@@ -41,3 +43,8 @@ int checkEndGame(const vector<vector<string>> &board);
 // returns 1 if player 1 (white) won
 // returns 2 if player 2 (black) won
 // returns 3 if the game is a draw
+
+// valid move takes the board as a parameter along with coordinates for the old
+// point and coordinates for the new point
+bool validMove(const vector<vector<string>> &board, int iOld, int jOld,
+               int iNew, int jNew, string color = "neutral");
