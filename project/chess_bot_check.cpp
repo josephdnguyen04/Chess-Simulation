212d2550b3f7b53aeb8e54d7c72f41808eb962be
sandke <sandke@cs.wallawalla.edu>
2023-03-10 04:42:39 +0000

    autosave

--- project/chess_bot_check.cpp
+++ project/chess_bot_check.cpp
@@ -22,48 +22,67 @@ vector<tuple<int, int>> getmoves(std::vector<std::vector<string>> board,
     king_moves = king.possibleMoves(board, current_cell);
     return king_moves;
   } break;
-  case 'N':{
+  case 'N': {
     Knight knight(piece);
     vector<tuple<int, int>> knight_moves;
     knight_moves = knight.possibleMoves(board, current_cell);
     return knight_moves;
   } break;
-  case 'Q':{
+  case 'Q': {
     Queen queen(piece);
     vector<tuple<int, int>> queen_moves;
     queen_moves = queen.possibleMoves(board, current_cell);
     return queen_moves;
   } break;
-  case 'R':{
+  case 'R': {
     Rook rook(piece);
     vector<tuple<int, int>> rook_moves;
     rook_moves = rook.possibleMoves(board, current_cell);
     return rook_moves;
   } break;
-  case 'B':{
+  case 'B': {
     Bishop bishop(piece);
     vector<tuple<int, int>> bishop_moves;
     bishop_moves = bishop.possibleMoves(board, current_cell);
     return bishop_moves;
   } break;
-  default:
-    {
+  default: {
     vector<tuple<int, int>> moves;
-    moves.push_back(make_tuple(0,0));
+    moves.push_back(make_tuple(0, 0));
     return moves;
-    }
-    break;
+  } break;
   }
 }
 
-// int isCheck(vector<vector<string>> board) {
-//   vector<tuple<int, int>> entire_moves;
-//   for (int row = 0; row < 8; row++) {
-//     for (int col = 0; col < 8; col++) {
-//       if (board.at(row).at(col) != "  ") {
-//         vector<tuple<int, int>> temporary;
-//         temporary = getmoves(board, make_tuple(row, col), board.at(row).at(col));
-//       }
-//     }
-//   }
-// }
+int isCheck(vector<vector<string>> board) {
+  vector<tuple<int, int>> entire_moves;
+  int check = 0;
+  tuple<int, int> black_king_pos, white_king_pos;
+
+  for (int row = 0; row < 8; row++) {
+    for (int col = 0; col < 8; col++) {
+      if (board.at(row).at(col) != "  ") {
+        if (board.at(row).at(col) != "wK")
+          white_king_pos = make_tuple(row, col);
+        if (board.at(row).at(col) != "bK")
+          black_king_pos = make_tuple(row, col);
+        vector<tuple<int, int>> temporary;
+        temporary =
+            getmoves(board, make_tuple(row, col), board.at(row).at(col));
+        for (auto aa : temporary) {
+          entire_moves.push_back(aa);
+        }
+      }
+    }
+  }
+  for (auto aa : entire_moves) {
+    if (aa == black_king_pos) {
+      check += 2;
+    }
+    if (aa == white_king_pos) {
+      check += 1;
+    }
+  }
+
+  return check;
+}
